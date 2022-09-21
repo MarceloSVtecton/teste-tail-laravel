a1d7d4dbdd9fc1cebac093c106642fe0c6d61866
Marcelo de Souza Vicente <mdesouzavicente475@gmail.com>
2022-09-21 15:17:18 -0300

    rotas

--- tw/routes/web.php
+++ tw/routes/web.php
@@ -27,7 +27,8 @@ Route::get('/materiais', 'ProdutosController@create', function() {
     return view('materiais');
 });
     
-Route::post('/materiais', 'ProdutosController@store')->name('cadastrar_material'); 
+Route::post('/materiais', 'ProdutosController@store')->name('cadastrar_material');
+
 Route::get('produtivos','ProdutivosController@index');
 Route::post('produtivos', 'ProdutivosController@store');
 Route::get('produtivos/{produtivo}', 'ProdutivosController@show');
@@ -36,11 +37,21 @@ Route::patch('produtivos/{produtivo}', 'ProdutivosController@update');
 Route::delete('produtivos/{produtivo}', 'ProdutivosController@destroy');
 
 
-
-
 Route::get('/apontamentos', function () {
     return view('apontamentos');
 });
+Route::get('os','osController@index');
+Route::post('os', 'osController@store');
+Route::get('os/{o}', 'osController@show');
+Route::get('os/{o}/edit', 'osController@edit');
+Route::patch('os/{o}', 'osController@update');
+Route::delete('os/{o}', 'osController@destroy');
+
+
+
+
+
+
 Route::get('/comissoes', function () {
     return view('comissoes');
 });
